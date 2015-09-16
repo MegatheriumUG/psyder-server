049362e4b1a1285e68ef84713705d7d451791b52
Martin Bories <martin.bories@megatherium.solutions>
2015-09-16 17:32:59 +0200

    POST UsergroupUpdate: created

--- controller/Usergroup.js
+++ controller/Usergroup.js
@@ -29,4 +29,43 @@ exports.setup = function(app) {
 			res.send({status: 'success', data: {usergroupId: usergroup._id}});
 		});
 	});
-}
+
+	/**
+	 * Bearbeitet eine Benutzergruppe.
+	 * Benötigt Administratorrechte.
+	 *
+	 * @param usergroupId	String	die ID der Benutzergruppe
+	 * @param label	String	der neue Name der Gruppe
+	 */
+	app.post('/UsergroupUpdate', function(req, res, jump) {
+		// Zugriff validieren
+		if (!res.locals.user) return res.send({error: 'Zugriff verweigert.'});
+		if (config.administrators.indexOf(res.locals.user._id.toString()) < 0) return res.send({error: 'Zugriff verweigert.'});
+
+		// Parameter abfragen
+		var usergroupId = req.body.usergroupId;
+		var label = req.body.label;
+
+		// Parameter validieren
+		if (!usergroupId) return res.send({error: 'Sie müssen die Benutzergruppe angeben.'});
+		if (!label) return res.send({error: 'Sie müssen einen Namen angeben.'});
+
+		// Benutzergruppe finden
+		Usergroup.findOne({_id: usergroupId})
+			.exec(function(err, usergroup) {
+				if (err) return jump(err);
+
+				// Benutzergruppe validieren
+				if (!usergroup) return res.send({error: 'Die Benutzergruppe konnte nicht gefunden werden.'});
+
+				// Benutzergruppe aktualisieren
+				usergroup.label = label;
+				usergroup.save(function(err) {
+					if (err) return jump(err);
+
+					// Antworten
+					res.send({status: 'success'});
+				});
+			});
+	});
+};
